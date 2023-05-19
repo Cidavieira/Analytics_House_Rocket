27cd95e243feef2674ea474843f907202ff11fb2
Cida Vieira <95623644+Cidavieira@users.noreply.github.com>
2023-05-19 11:42:55 -0300

    Update 2_insights.py

--- pages/2_insights.py
+++ pages/2_insights.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import plotly as plt
 import plotly.express as px
+
 from datetime import datetime,time
 
 st.set_page_config(page_title='Insights',layout='wide')
@@ -77,14 +79,14 @@ def hipotese5(data):
     st.subheader('H5: Imóveis com 3 banheiros tem um crescimento de MoM (Month over Month) de 15%.')
     st.subheader('H5: Falsa')
 
-    df1 = data.loc[data['bathrooms'] == 3, :].copy()
-    df1['month_2014'] = pd.DatetimeIndex(df1['date']).month
-    df1['year'] = pd.DatetimeIndex(df1['date']).year
-    df1 = df1.loc[data['year'] == 2014, :].copy()
-    df2 = df1[['price', 'month_2014']].groupby('month_2014').mean().reset_index()
+    #df1 = data.loc[data['bathrooms'] == 3, :].copy()
+    #df2 = df1.loc[data['year'] == int('2014'), 'price'].median()
+    #df3 = df1.loc[data['year'] == int('2015'), 'price'].median()
+
     # plot
-    fig=px.line(df2,x='month_2014',y='price')
-    st.plotly_chart(fig, use_container_width=True)
+    #data_plot = {'Porao': ['Sem_porao', 'Com_porao'], 'Area_total_media': [df1, df2]}
+    #fig=px.line(df5,x='floors',y='percentual')
+    #st.plotly_chart(fig, use_container_width=True)
 
 
     return None
@@ -100,7 +102,7 @@ def hipotese6(data):
     return None
 
 def hipotese7(data):
-    st.subheader('H7: Existem Imovéis sem banheiro, representam de 0.05% do dashbord.')
+    st.subheader('H7: Imovéis sem banheiros são raros representam menos de 0.05% do dashbord.')
     st.subheader('H7: Verdadeira')
     df5 = data[['id', 'bathrooms']].groupby('bathrooms').count().reset_index()
     df5['percentual'] = df5['id'] / len(data)
@@ -120,15 +122,14 @@ def hipotese8(data):
     return None
 
 def hipotese9(data):
-    st.subheader('H9: Aproximadamente Metade dos Imoveis disponíveis são bons candidatos a dar lucro.')
+    st.subheader('H9: Metade dos Imoveis disponíveis são bons candidatos a dar lucro.')
     st.subheader('H9: Verdadeira')
 
-    status = pd.DataFrame({'status': ['compra', 'nao compra'],
-                           'id': [10579, 11034]})
-
-    #plot
-    fig = px.bar(status, x='status', y='id',title='Imóveis candidatos a dar lucro')
-    st.plotly_chart(fig, use_container_width=True)
+   # status = relatorio1.loc[:, ['status', 'id']].groupby('status').count().reset_index()
+   # plt.bar(status['status'], status['id'])
+    # plot
+    #fig = px.bar(status, x='status', y='id',title='Imóveis candidatos a dar lucro')
+    #st.plotly_chart(fig, use_container_width=True)
 
     return None
 
@@ -159,47 +160,85 @@ st.sidebar.markdown('# House Rocket')
 st.sidebar.markdown("""---""")
 st.sidebar.markdown('### Powered by Aparecida Vieira')
 
+# ===========================================
+# Layout no streamlit
+# ===========================================
+
+#with st.container():
+    #st.title("Overal Metrics")
+
+    #col1, col2, col3, col4, col5, col6 = st.columns(6)
+    #col1=st.columns(1)
+    #with col1:
+        #imoveis_unicos = df['id'].nunique()
+        #ol1.metric('Imóveis', imoveis_unicos)
+        #with col2:
+            #avg_distance = distance(df1, fig=False)
+            #col2.metric('A dist média entregas', avg_distance)
+
+        #with col3:
+            #festivais = avg_std_time_delivery(df1, 'Yes', 'avg_time')
+            #col3.metric('Tempo médio c/ fest', festivais)
+        #with col4:
+            #festivais = avg_std_time_delivery(df1, 'Yes', 'std_time')
+            #col4.metric('Std entrega c/ fest', festivais)
+        #with col5:
+            #festivais = avg_std_time_delivery(df1, 'No', 'avg_time')
+            #col5.metric('Tempo médio s/ fest', festivais)
+        #with col6:
+            #festivais = avg_std_time_delivery(df1, 'No', 'std_time')
+            #col6.metric('Std entrega s/ fest', festivais)
+
 with st.container():
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     data = set_feature(df)
     hipotese1(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese2(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese3(df)
 
+
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese4(df)
 
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese5(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese6(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese7(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese8(df)
 
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese9(df)
 
 with st.container():
-    st.markdown("""---""")
+    #st.markdown("""---""")
+    #st.title("Tempo Médio de entrega por cidade")
     hipotese10(df)
-
-
-
